13bf15ff8136556abace42d0348ed45ce20cdf2e
kerryosity <gogeechannel@gmail.com>
2021-03-14 15:53:48 +0300

    make homework-3

--- pokemon-game/src/components/Layout/index.js
+++ pokemon-game/src/components/Layout/index.js
@@ -1,18 +1,20 @@
 import s from './style.module.css';
 
-const Layout = ({ title, descr, urlBg, colorBg }) => {
-    const inline_style = urlBg ? { backgroundImage: `url(${urlBg})` } : { backgroundColor: `${colorBg}` }
+const Layout = ({ title, urlBg, colorBg, children }) => {
+    let style = {};
+    if (urlBg) { style.backgroundImage = `url(${urlBg})` }
+    if (colorBg) { style.backgroundColor = colorBg }
 
     return (
-        <section className={s.root} style={inline_style}>
+        <section className={s.root} style={style}>
             <div className={s.wrapper}>
                 <article>
                     <div className={s.title}>
                         <h3>{title}</h3>
                         <span className={s.separator}></span>
                     </div>
-                    <div className={s.full + ' ' + s.desc}>
-                        <p>{descr}</p>
+                    <div className={`${s.full} ${s.desc}`}>
+                        {children}
                     </div>
                 </article>
             </div>
